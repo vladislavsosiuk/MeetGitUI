1ee24375a1ab7b37c3425faeeb2fbe3e0c4be639
Vladislav Sosiuk <vladislavsosiuk@gmail.com>
2018-11-29 09:53:03 +0200

    Create Comment model

---
+++ TestSourceTree/TestSourceTree/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSourceTree.Models
+{
+    public class Comment
+    {
+
+        [JsonProperty("postId")]
+        public int PostId { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
