804a1b7f57112187925e4d9e83e3fc638d0d06ac
Vladislav Sosiuk <vladislavsosiuk@gmail.com>
2018-11-29 09:53:47 +0200

    Download data in main

--- TestSourceTree/TestSourceTree/Program.cs
+++ TestSourceTree/TestSourceTree/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using TestSourceTree.Models;
 
 namespace TestSourceTree
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
+            WebClient client = new WebClient();
+            string result = await client.DownloadStringTaskAsync("https://jsonplaceholder.typicode.com/comments");
+            List<Comment> comments = JsonConvert.DeserializeObject<List<Comment>>(result);
+            foreach (var comment in comments)
+            {
+                Console.WriteLine(comment.Name);
+            }
         }
     }
 }
